25e7edffcde363eb5d5aad8a8869a36ca42853b5
Amit <kumaraj1886@gmail.com>
2025-07-27 15:12:28 +0530

    Update urls.py

--- backend/travel_system/urls.py
+++ backend/travel_system/urls.py
@@ -1,8 +1,9 @@
-
+# travel_system/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('travel_system.apps.accounts.urls')),  # <-- root now mapped
     path('admin/', admin.site.urls),
     path('accounts/', include('travel_system.apps.accounts.urls')),
     path('bookings/', include('travel_system.apps.bookings.urls')),
