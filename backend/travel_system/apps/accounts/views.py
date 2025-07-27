9c0a86b2958823a992f109bd5e298bbd3d3697dd
Amit <kumaraj1886@gmail.com>
2025-07-27 15:03:41 +0530

    Update views.py

--- backend/travel_system/apps/accounts/views.py
+++ backend/travel_system/apps/accounts/views.py
@@ -1,1 +1,6 @@
 # TODO: implement views
+# travel_system/apps/accounts/views.py
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>Welcome to Travel Booking System</h1>")
